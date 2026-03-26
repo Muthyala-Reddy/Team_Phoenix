3cd2031fe1b4c46cd9b7e0fb5fad08dfcf668dcb
Dikshant Singh <dikshant.singh@insightglobal.com>
2026-03-26 13:29:08 +0530

    my life my rules

--- src/main/java/com/demo/spring/controllers/TicketRestController.java
+++ src/main/java/com/demo/spring/controllers/TicketRestController.java
@@ -1,6 +1,7 @@
 package com.demo.spring.controllers;
 
 import com.demo.spring.entity.Ticket;
+import com.demo.spring.services.TicketService;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
--- src/main/java/com/demo/spring/repositories/TicketRepository.java
+++ src/main/java/com/demo/spring/repositories/TicketRepository.java
@@ -1,5 +1,6 @@
 package com.demo.spring.repositories;
 
+import com.demo.spring.entity.Ticket;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface TicketRepository extends JpaRepository<Ticket,Integer>
