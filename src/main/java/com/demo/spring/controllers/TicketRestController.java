496ca0a0409d78502b3eae75f1556226ff98713a
Miraa1510 <Sreemiraa.Tn@insightglobal.com>
2026-03-26 12:56:25 +0530

    Entity class file added

---
+++ src/main/java/com/demo/spring/controllers/TicketRestController.java
@@ -0,0 +1,24 @@
+package com.demo.spring.controllers;
+
+import com.demo.spring.entity.Ticket;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.List;
+
+public class TicketRestController {
+
+    private TicketService ticketService;
+
+    public TicketRestController(TicketService ticketService) {
+        this.ticketService = ticketService;
+    }
+
+    @GetMapping(path = "/",produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<List<Ticket>> findAllTickets(){
+        return ResponseEntity.of(ticketService.getAllTickets());
+    }
+
+
+}
