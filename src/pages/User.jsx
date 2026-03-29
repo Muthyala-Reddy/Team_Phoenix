0a51545f63615d8d4485316a3dc23d92ce8d24b4
Dikshant Singh <dikshant.singh@insightglobal.com>
2026-03-29 20:17:50 +0530

    Add frontend application

---
+++ src/pages/User.jsx
@@ -0,0 +1,78 @@
+import React, { useState } from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
+import { useNavigate } from "react-router-dom";
+import axios from "axios";
+import "../styles/Users.css";
+
+function User() {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const [error, setError] = useState("");
+  const navigate = useNavigate();
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    setError("");
+
+    try {
+      const res = await axios.post("http://localhost:8082/api/auth/login", {
+        username,
+        password,
+      });
+
+      if (res.status === 200) {
+        localStorage.setItem("username", res.data.username);
+        localStorage.setItem("role", res.data.role);
+
+        if (res.data.role === "ADMIN") navigate("/admin");
+        else navigate("/user");
+      }
+    } catch (err) {
+      if (err.response && err.response.status === 401) {
+        setError("Invalid username or password");
+      } else {
+        setError("Server error. Please try again.");
+      }
+    }
+  };
+
+  return (
+    <div className="user-page">
+      <div className="user-card shadow">
+        <h3 className="text-center mb-4">User Login</h3>
+
+        {error && <p className="text-danger text-center">{error}</p>}
+
+        <form onSubmit={handleSubmit}>
+          <div className="mb-3">
+            <label className="form-label">Username</label>
+            <input
+              type="text"
+              className="form-control form-control-sm"
+              value={username}
+              onChange={(e) => setUsername(e.target.value)}
+              required
+            />
+          </div>
+
+          <div className="mb-3">
+            <label className="form-label">Password</label>
+            <input
+              type="password"
+              className="form-control form-control-sm"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+              required
+            />
+          </div>
+
+          <button type="submit" className="btn btn-success w-100">
+            Submit
+          </button>
+        </form>
+      </div>
+    </div>
+  );
+}
+
+export default User;
